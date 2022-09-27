b6cc5e879975cdadb2f9ecf0020f746598b6f2c2
Pipo <eliaducceschi@hotmail.it>
2022-09-27 15:44:32 +0200

    Change display fps to use parent

--- wle_locomotion/js/cauldron/display_fps.js
+++ wle_locomotion/js/cauldron/display_fps.js
@@ -8,9 +8,19 @@ WL.registerComponent('display-fps', {
         this._myTotalDT = 0;
         this._myFrames = 0;
 
-        this._myFPS = 0;
+        this._myVisualFPSParent = this.object.pp_addObject();
 
-        this._myTextMaterial = PP.myDefaultResources.myMaterials.myText.clone();
+        let visualParams = new PP.VisualTextParams();
+        visualParams.myText = "0";
+
+        visualParams.myTransform.mat4_setPositionRotationScale([-0.115, -0.115, 0.35], [0, 180, 0], [0.3, 0.3, 0.3]);
+
+        visualParams.myMaterial = PP.myDefaultResources.myMaterials.myText.clone();
+        visualParams.myMaterial.color = [0, 1, 0, 1];
+
+        visualParams.myParent = this._myVisualFPSParent;
+
+        this._myVisualFPS = new PP.VisualText(visualParams);
 
         //PP.myEasyTuneVariables.add(new PP.EasyTuneNumber("Z", 0.35, 0.1, 3));
         //PP.myEasyTuneVariables.add(new PP.EasyTuneNumber("Y", -0.115, 0.1, 3));
@@ -24,27 +34,15 @@ WL.registerComponent('display-fps', {
         if (this._myTimer.isDone()) {
             this._myTimer.start();
 
-            this._myFPS = Math.round(this._myFrames / this._myTotalDT);
+            let fps = Math.round(this._myFrames / this._myTotalDT);
+
+            this._myVisualFPS.getParams().myText = fps.toFixed(0);
+            this._myVisualFPS.paramsUpdated();
 
             this._myTotalDT = 0;
             this._myFrames = 0;
         }
 
-        let head = PP.myPlayerObjects.myHead;
-        let headPosition = head.pp_getPosition();
-        let textPosition = headPosition.vec3_add(head.pp_getForward().vec3_scale(0.35))
-            .vec3_add(head.pp_getUp().vec3_scale(-0.115)).vec3_add(head.pp_getRight().vec3_scale(0.115));
-
-        let visualParams = new PP.VisualTextParams();
-        visualParams.myText = this._myFPS.toFixed(0);
-
-        let rotation = PP.quat_create();
-        rotation.quat_setForward(head.pp_getForward().vec3_negate(), head.pp_getUp());
-        visualParams.myTransform.mat4_setPositionRotationQuatScale(textPosition, rotation, [0.3, 0.3, 0.3]);
-
-        visualParams.myMaterial = this._myTextMaterial;
-        visualParams.myMaterial.color = [0, 1, 0, 1];
-
-        PP.myVisualManager.draw(visualParams);
+        this._myVisualFPSParent.pp_setTransformQuat(PP.myPlayerObjects.myHead.pp_getTransformQuat());
     }
 });
