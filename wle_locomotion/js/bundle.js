af0328b7a1cbe93ab9008084dc6505563e22468c
Pipo <eliaducceschi@hotmail.it>
2023-02-21 15:25:49 +0100

    Add generic function counter

--- wle_locomotion/js/bundle.js
+++ wle_locomotion/js/bundle.js
@@ -106,6 +106,7 @@ require('./pp/debug/function_calls_counter/debug_function_calls_count_logger');
 require('./pp/debug/function_calls_counter/components/debug_pp_array_creation_calls_counter');
 require('./pp/debug/function_calls_counter/components/debug_array_function_calls_counter');
 require('./pp/debug/function_calls_counter/components/debug_pp_function_calls_counter');
+require('./pp/debug/function_calls_counter/components/debug_function_calls_counter_component');
 
 //	GAMEPLAY
 
