fdfa0546bc7e314cdbc0a1cf2357d45a8426fc9a
Pipo <eliaducceschi@hotmail.it>
2023-02-28 12:49:27 +0100

    Add experimental locomotion to pp

---
+++ wle_locomotion/js/pp/gameplay/experimental/locomotion/legacy/locomotion/player_locomotion_movement.js
@@ -0,0 +1,30 @@
+
+
+PlayerLocomotionMovementRuntimeParams = class PlayerLocomotionMovementRuntimeParams {
+    constructor() {
+        this.myIsFlying = false;
+        this.myCollisionRuntimeParams = null;
+
+        this.myIsTeleportDetecting = false;
+        this.myIsTeleporting = false;
+        this.myTeleportJustPerformed = false;
+    }
+};
+
+PlayerLocomotionMovement = class PlayerLocomotionMovement {
+    constructor(locomotionRuntimeParams) {
+        this._myLocomotionRuntimeParams = locomotionRuntimeParams;
+    }
+
+    start() {
+
+    }
+
+    stop() {
+
+    }
+
+    canStop() {
+        return true;
+    }
+};
