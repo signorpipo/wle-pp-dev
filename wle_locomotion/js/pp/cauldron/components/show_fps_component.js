f0d891e3a90af0883ca60a8f5b31b50e50701732
Pipo <eliaducceschi@hotmail.it>
2023-05-26 16:46:15 +0200

    Disable fps component when debug not enabled

--- wle_locomotion/js/pp/cauldron/components/show_fps_component.js
+++ wle_locomotion/js/pp/cauldron/components/show_fps_component.js
@@ -13,32 +13,38 @@ export class ShowFPSComponent extends Component {
     };
 
     start() {
-        this._myTimer = new Timer(this._myRefreshSeconds);
-        this._myTotalDT = 0;
-        this._myFrames = 0;
+        this._myStarted = false;
 
-        this._myVisualFPSParent = this.object.pp_addObject();
+        if (Globals.isDebugEnabled(this.engine)) {
+            this._myTimer = new Timer(this._myRefreshSeconds);
+            this._myTotalDT = 0;
+            this._myFrames = 0;
 
-        let visualParams = new VisualTextParams(this.engine);
-        visualParams.myText = "00";
+            this._myVisualFPSParent = this.object.pp_addObject();
 
-        visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.115, -0.115, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+            let visualParams = new VisualTextParams(this.engine);
+            visualParams.myText = "00";
 
-        if (this._myTextMaterial != null) {
-            visualParams.myMaterial = this._myTextMaterial.clone();
-        } else {
-            visualParams.myMaterial = Globals.getDefaultMaterials(this.engine).myText.clone();
-            visualParams.myMaterial.color = vec4_create(1, 1, 1, 1);
+            visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.115, -0.115, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+
+            if (this._myTextMaterial != null) {
+                visualParams.myMaterial = this._myTextMaterial.clone();
+            } else {
+                visualParams.myMaterial = Globals.getDefaultMaterials(this.engine).myText.clone();
+                visualParams.myMaterial.color = vec4_create(1, 1, 1, 1);
+            }
+
+            visualParams.myParent = this._myVisualFPSParent;
+            visualParams.myLocal = true;
+
+            this._myVisualFPS = new VisualText(visualParams);
+
+            //Globals.getEasyTuneVariables(this.engine).add(new EasyTuneNumber("FPS X", -0.25, 0.1, 3, undefined, undefined, this.engine));
+            //Globals.getEasyTuneVariables(this.engine).add(new EasyTuneNumber("FPS Y", -0.130, 0.1, 3, undefined, undefined, this.engine));
+            //Globals.getEasyTuneVariables(this.engine).add(new EasyTuneNumber("FPS Z", 0.35, 0.1, 3, undefined, undefined, this.engine));
+
+            this._myStarted = true;
         }
-
-        visualParams.myParent = this._myVisualFPSParent;
-        visualParams.myLocal = true;
-
-        this._myVisualFPS = new VisualText(visualParams);
-
-        //Globals.getEasyTuneVariables(this.engine).add(new EasyTuneNumber("FPS X", -0.25, 0.1, 3, undefined, undefined, this.engine));
-        //Globals.getEasyTuneVariables(this.engine).add(new EasyTuneNumber("FPS Y", -0.130, 0.1, 3, undefined, undefined, this.engine));
-        //Globals.getEasyTuneVariables(this.engine).add(new EasyTuneNumber("FPS Z", 0.35, 0.1, 3, undefined, undefined, this.engine));
     }
 
     update(dt) {
@@ -46,7 +52,9 @@ export class ShowFPSComponent extends Component {
     }
 
     onDestroy() {
-        this._myVisualFPS.destroy();
+        if (this._myStarted) {
+            this._myVisualFPS.destroy();
+        }
     }
 }
 
@@ -57,32 +65,34 @@ export class ShowFPSComponent extends Component {
 ShowFPSComponent.prototype.update = function () {
     let playerTransformQuat = quat2_create();
     return function update(dt) {
-        this._myTotalDT += dt;
-        this._myFrames++;
+        if (this._myStarted) {
+            this._myTotalDT += dt;
+            this._myFrames++;
 
-        this._myTimer.update(dt);
-        if (this._myTimer.isDone()) {
-            this._myTimer.start();
+            this._myTimer.update(dt);
+            if (this._myTimer.isDone()) {
+                this._myTimer.start();
 
-            let fps = Math.round(this._myFrames / this._myTotalDT);
+                let fps = Math.round(this._myFrames / this._myTotalDT);
 
-            let visualParams = this._myVisualFPS.getParams();
+                let visualParams = this._myVisualFPS.getParams();
 
-            if (XRUtils.isSessionActive(this.engine)) {
-                visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.115, -0.115, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
-            } else {
-                visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.25, -0.130, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+                if (XRUtils.isSessionActive(this.engine)) {
+                    visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.115, -0.115, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+                } else {
+                    visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.25, -0.130, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+                }
+
+                //visualParams.myTransform.mat4_setPositionRotationScale([Globals.getEasyTuneVariables(this.engine).get("FPS X"), Globals.getEasyTuneVariables(this.engine).get("FPS Y"), Globals.getEasyTuneVariables(this.engine).get("FPS Z")], vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+
+                visualParams.myText = fps.toFixed(0);
+                this._myVisualFPS.paramsUpdated();
+
+                this._myTotalDT = 0;
+                this._myFrames = 0;
             }
 
-            //visualParams.myTransform.mat4_setPositionRotationScale([Globals.getEasyTuneVariables(this.engine).get("FPS X"), Globals.getEasyTuneVariables(this.engine).get("FPS Y"), Globals.getEasyTuneVariables(this.engine).get("FPS Z")], vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
-
-            visualParams.myText = fps.toFixed(0);
-            this._myVisualFPS.paramsUpdated();
-
-            this._myTotalDT = 0;
-            this._myFrames = 0;
+            this._myVisualFPSParent.pp_setTransformQuat(Globals.getPlayerObjects(this.engine).myHead.pp_getTransformQuat(playerTransformQuat));
         }
-
-        this._myVisualFPSParent.pp_setTransformQuat(Globals.getPlayerObjects(this.engine).myHead.pp_getTransformQuat(playerTransformQuat));
     };
 }();
