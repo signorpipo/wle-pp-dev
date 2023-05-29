f8032733f19865261a9ec1ba14288825846428cd
Pipo <eliaducceschi@hotmail.it>
2023-05-29 16:41:00 +0200

    Add offsets to show fps

--- wle_locomotion/js/pp/cauldron/components/show_fps_component.js
+++ wle_locomotion/js/pp/cauldron/components/show_fps_component.js
@@ -1,5 +1,5 @@
 import { Component, Property } from "@wonderlandengine/api";
-import { quat2_create, vec3_create, vec4_create } from "../../plugin/js/extensions/array_extension";
+import { quat2_create, quat_create, vec3_create, vec4_create } from "../../plugin/js/extensions/array_extension";
 import { Globals } from "../../pp/globals";
 import { Timer } from "../cauldron/timer";
 import { XRUtils } from "../utils/xr_utils";
@@ -8,6 +8,10 @@ import { VisualText, VisualTextParams } from "../visual/elements/visual_text";
 export class ShowFPSComponent extends Component {
     static TypeName = "pp-show-fps";
     static Properties = {
+        _myLookAtPlayer: Property.bool(true),
+        _myOffsetX: Property.float(0),
+        _myOffsetY: Property.float(0),
+        _myOffsetZ: Property.float(0),
         _myRefreshSeconds: Property.float(0.25),
         _myTextMaterial: Property.material()
     };
@@ -25,7 +29,7 @@ export class ShowFPSComponent extends Component {
             let visualParams = new VisualTextParams(this.engine);
             visualParams.myText = "00";
 
-            visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.115, -0.115, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+            visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.25 - this._myOffsetX, -0.130 + this._myOffsetY, 0.35 + this._myOffsetZ), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
 
             if (this._myTextMaterial != null) {
                 visualParams.myMaterial = this._myTextMaterial.clone();
@@ -63,7 +67,10 @@ export class ShowFPSComponent extends Component {
 // IMPLEMENTATION
 
 ShowFPSComponent.prototype.update = function () {
-    let playerTransformQuat = quat2_create();
+    let playerHeadTransformQuat = quat2_create();
+    let fpsRotationQuat = quat_create();
+    let up = vec3_create(0, 1, 0);
+    let lookToForward = vec3_create();
     return function update(dt) {
         if (this._myStarted) {
             this._myTotalDT += dt;
@@ -78,9 +85,16 @@ ShowFPSComponent.prototype.update = function () {
                 let visualParams = this._myVisualFPS.getParams();
 
                 if (XRUtils.isSessionActive(this.engine)) {
-                    visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.115, -0.115, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+                    visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.115 - this._myOffsetX, -0.115 + this._myOffsetY, 0.35 + this._myOffsetZ), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+
+                    if (this._myLookAtPlayer) {
+                        lookToForward = visualParams.myTransform.mat4_getPosition(lookToForward).vec3_negate(lookToForward).vec3_normalize(lookToForward);
+                        fpsRotationQuat = visualParams.myTransform.mat4_getRotationQuat(fpsRotationQuat);
+                        fpsRotationQuat.quat_setForward(lookToForward, up, fpsRotationQuat);
+                        visualParams.myTransform.mat4_setRotationQuat(fpsRotationQuat);
+                    }
                 } else {
-                    visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.25, -0.130, 0.35), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
+                    visualParams.myTransform.mat4_setPositionRotationScale(vec3_create(-0.25 - this._myOffsetX, -0.130 + this._myOffsetY, 0.35 + this._myOffsetZ), vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
                 }
 
                 //visualParams.myTransform.mat4_setPositionRotationScale([Globals.getEasyTuneVariables(this.engine).get("FPS X"), Globals.getEasyTuneVariables(this.engine).get("FPS Y"), Globals.getEasyTuneVariables(this.engine).get("FPS Z")], vec3_create(0, 180, 0), vec3_create(0.3, 0.3, 0.3));
@@ -92,7 +106,8 @@ ShowFPSComponent.prototype.update = function () {
                 this._myFrames = 0;
             }
 
-            this._myVisualFPSParent.pp_setTransformQuat(Globals.getPlayerObjects(this.engine).myHead.pp_getTransformQuat(playerTransformQuat));
+            playerHeadTransformQuat = Globals.getPlayerObjects(this.engine).myHead.pp_getTransformQuat(playerHeadTransformQuat);
+            this._myVisualFPSParent.pp_setTransformQuat(playerHeadTransformQuat);
         }
     };
 }();
