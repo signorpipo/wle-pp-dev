23bc5c882f96ba5571a69a6ce2f3935f8b809eba
Pipo <eliaducceschi@hotmail.it>
2023-08-09 16:48:13 +0200

    Add open link on click

--- wle_pp_dev/js/pp/cauldron/utils/browser_utils.js
+++ wle_pp_dev/js/pp/cauldron/utils/browser_utils.js
@@ -13,13 +13,7 @@ export function isDesktop(engine = Globals.getMainEngine()) {
     return !BrowserUtils.isMobile(engine);
 }
 
-export function openLink(url, newTab = true, exitXRSession = true, onSuccessCallback = null, onFailureCallback = null, engine = Globals.getMainEngine()) {
-    if (exitXRSession) {
-        XRUtils.exitSession(engine);
-    }
-
-    let document = Globals.getDocument(engine);
-
+export function openLink(url, newTab = true, exitXRSessionBeforeOpen = true, exitXRSessionOnSuccess = true, tryOpenLinkOnClickOnFailure = false, onSuccessCallback = null, onFailureCallback = null, engine = Globals.getMainEngine()) {
     let element = document.createElement("a");
 
     element.style.display = "none";
@@ -30,11 +24,56 @@ export function openLink(url, newTab = true, exitXRSession = true, onSuccessCall
         let targetPage = undefined;
         if (newTab) {
             targetPage = "_blank";
+        } else {
+            targetPage = "_top";
         }
 
         let result = window.open(url, targetPage);
 
         if (result != null) {
+            if (!exitXRSessionBeforeOpen && exitXRSessionOnSuccess) {
+                XRUtils.exitSession(engine);
+            }
+
+            if (onSuccessCallback != null) {
+                onSuccessCallback();
+            }
+        } else {
+            if (tryOpenLinkOnClickOnFailure) {
+                setTimeout(function () {
+                    BrowserUtils.openLinkOnClick(url, newTab, exitXRSessionOnSuccess, onSuccessCallback, onFailureCallback);
+                }, 100);
+            } else if (onFailureCallback != null) {
+                onFailureCallback();
+            }
+        }
+    });
+
+    if (exitXRSessionBeforeOpen) {
+        XRUtils.exitSession(engine);
+    }
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+export function openLinkOnClick(url, newTab = true, exitXRSessionOnSuccess = true, onSuccessCallback = null, onFailureCallback = null, engine = Globals.getMainEngine()) {
+    document.addEventListener("click", function () {
+        let targetPage = undefined;
+        if (newTab) {
+            targetPage = "_blank";
+        } else {
+            targetPage = "_top";
+        }
+
+        let result = window.open(url, targetPage);
+
+        if (result != null) {
+            if (exitXRSessionOnSuccess) {
+                XRUtils.exitSession(engine);
+            }
+
             if (onSuccessCallback != null) {
                 onSuccessCallback();
             }
@@ -43,35 +82,12 @@ export function openLink(url, newTab = true, exitXRSession = true, onSuccessCall
                 onFailureCallback();
             }
         }
-    });
-
-    element.click();
-
-    document.body.removeChild(element);
-}
-
-export function openLinkPersistent(url, newTab = true, exitXRSession = true, timeOutSeconds = 15, onSuccessCallback = null, onFailureCallback = null, engine = Globals.getMainEngine()) {
-    let totalSeconds = 0;
-    let secondsTillNextAttempt = 0.5;
-    let onPersistentFailureCallback = function (...args) {
-        if (timeOutSeconds != null && totalSeconds >= timeOutSeconds) {
-            if (onFailureCallback != null) {
-                onFailureCallback(...args);
-            }
-        } else {
-            totalSeconds += secondsTillNextAttempt;
-            setTimeout(function () {
-                BrowserUtils.openLink(url, newTab, exitXRSession, onSuccessCallback, onPersistentFailureCallback, engine);
-            }, secondsTillNextAttempt * 1000);
-        }
-    };
-
-    BrowserUtils.openLink(url, newTab, exitXRSession, onSuccessCallback, onPersistentFailureCallback, engine);
+    }, { once: true });
 }
 
 export let BrowserUtils = {
     isMobile,
     isDesktop,
     openLink,
-    openLinkPersistent
+    openLinkOnClick
 };
