6b5b6520b9c85257efdfc8e33dbe22cbbbec09cb
Pipo <eliaducceschi@hotmail.it>
2023-08-21 14:42:41 +0200

    Add eslint

---
+++ wle_pp_dev/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname
+    },
+    env: {
+        browser: true
+    },
+    globals: {
+    },
+    plugins: [
+        "@typescript-eslint/eslint-plugin",
+        "deprecation"
+    ],
+    extends: [
+        "eslint:recommended"
+    ],
+    rules: {
+        "deprecation/deprecation": "warn",
+        "no-unused-vars": ["warn", { "args": "none", "varsIgnorePattern": "^_" }]
+    },
+    ignorePatterns: [
+        "/node_modules/", "/deploy/", "/cache/", "/.editor/", "/languages/", "/.editor/", "/assets/", "/static/", "/**/.eslintrc.cjs"
+    ],
+    overrides: [
+    ]
+};
