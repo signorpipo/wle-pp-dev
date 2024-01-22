f0a8bb4c22f6e8a4db214daa6317648db9be2f3e
Elia <eliaducceschi@hotmail.it>
2024-01-22 09:49:16 +0100

    Add clean script

---
+++ wle_pp_dev/dev/wle-cleaner-editor-bundle-extras.js
@@ -0,0 +1,18 @@
+const window = {
+    navigator: {},
+    location: {}
+};
+
+class URL { }
+
+const HowlerGlobal = {
+    prototype: {}
+};
+
+const Howl = {
+    prototype: {}
+};
+
+const Sound = {
+    prototype: {}
+};
