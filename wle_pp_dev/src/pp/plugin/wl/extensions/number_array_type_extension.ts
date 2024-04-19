feef581f741906ae6f53c58b9ca272759a0372f8
Elia <eliaducceschi@hotmail.it>
2024-04-19 17:41:01 +0200

    Add vec_create

--- wle_pp_dev/src/pp/plugin/wl/extensions/number_array_type_extension.ts
+++ wle_pp_dev/src/pp/plugin/wl/extensions/number_array_type_extension.ts
@@ -1,4 +1,4 @@
-import { ArrayLike } from "wle-pp/cauldron/type_definitions/array_type_definitions.js";
+import { ArrayLike } from "../../../cauldron/type_definitions/array_type_definitions.js";
 
 declare module "@wonderlandengine/api" {
     export interface NumberArray extends ArrayLike<number> { }
