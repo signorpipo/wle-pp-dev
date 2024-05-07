d601b467fb96cd2dac69906bb4e534a61aa32d08
Elia <eliaducceschi@hotmail.it>
2024-05-07 15:56:54 +0200

    Give a bit of time to fade out obscure

--- wle_pp_dev/src/pp/gameplay/experimental/locomotion/legacy/locomotion/player_locomotion.ts
+++ wle_pp_dev/src/pp/gameplay/experimental/locomotion/legacy/locomotion/player_locomotion.ts
@@ -453,7 +453,7 @@ export class PlayerLocomotion {
                 params.myObscureMaterial = null;
                 params.myObscureRadius = 0.5;
 
-                params.myObscureFadeOutSeconds = 0;
+                params.myObscureFadeOutSeconds = 0.1;
                 params.myObscureFadeInSeconds = 0.25;
 
                 params.myObscureFadeEasingFunction = EasingFunction.linear;
