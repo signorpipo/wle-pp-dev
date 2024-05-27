aecbcfdb7f86cca5a76019e2feb40ced924cc103
Pipo <eliaducceschi@hotmail.it>
2024-05-27 15:54:41 +0200

    Add tests

--- wle_pp_dev/src/test/types/array_type_definition_tests.ts
+++ wle_pp_dev/src/test/types/array_type_definition_tests.ts
@@ -1,4 +1,5 @@
 import { type NumberArray } from "@wonderlandengine/api";
+import { first } from "wle-pp/cauldron/utils/array/array_utils.js";
 import { ArrayLike, DynamicArrayLike, Vector3 } from "../../pp/cauldron/type_definitions/array_type_definitions.js";
 
 function __test(): void {
@@ -21,6 +22,7 @@ function __test(): void {
     arrayLikeFromArray.indexOf(0);
     vector3FromArray.indexOf(0);
 
+    first(numberArray);
     numberArray.pp_first();
     vector3FromArray.pp_first();
     arrayLikeFromFloat32.pp_first();
@@ -43,6 +45,9 @@ function __test(): void {
     vector3FromArray.vec_equals(vector3FromArray);
     vector3FromArray.vec_equals(vector3FromFloat32);
     vector3FromArray.vec_equals(dynamicArrayLikeFromArray);
+
+    vector3FromArray.pp_clone();
+    vector3FromArray.vec_clone();
 
     // vector3FromArray.vec_equals(stringArray);
 
