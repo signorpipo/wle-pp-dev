af817e061990baf97db9da7b81f36cfbc0f45e2a
Pipo <eliaducceschi@hotmail.it>
2024-06-02 16:50:23 +0200

    Adjust extensions

--- wle_pp_dev/src/pp/plugin/js/extensions/array/vec_type_extension.ts
+++ wle_pp_dev/src/pp/plugin/js/extensions/array/vec_type_extension.ts
@@ -1,10 +1,9 @@
+import { Vector } from "../../../../cauldron/type_definitions/array_type_definitions.js";
+
 /**
  * #WARN this type extension is actually added at runtime only if you call `initVecExtension`  
  * The `initPP` function, which is automatically called by the `pp-gateway` component, does this for you
  */
-
-import { Vector } from "../../../../cauldron/type_definitions/array_type_definitions.js";
-
 export interface VectorExtension<VectorType extends Vector> {
     vec_set<T extends VectorType>(this: T, uniformValue: number): this;
     vec_set<T extends VectorType>(this: T, firstValue: number, ...remainingValues: number[]): this;
