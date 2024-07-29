dce4737edba480e14f1ae52857337d4d8a9579d1
Pipo <eliaducceschi@hotmail.it>
2024-07-29 11:53:14 +0200

    Specify delay before switching to tracked hand if gamepad is available

--- wle_pp_dev/src/pp/input/pose/hand_pose.js
+++ wle_pp_dev/src/pp/input/pose/hand_pose.js
@@ -1,3 +1,4 @@
+import { Timer } from "../../cauldron/cauldron/timer.js";
 import { XRUtils } from "../../cauldron/utils/xr_utils.js";
 import { quat_create, vec3_create } from "../../plugin/js/extensions/array/vec_create_extension.js";
 import { Handedness, InputSourceType } from "../cauldron/input_types.js";
@@ -9,6 +10,7 @@ export class HandPoseParams extends BasePoseParams {
     constructor(engine) {
         super(engine);
 
+        this.mySwitchToTrackedHandDelay = 0;
         this.myFixTrackedHandRotation = true;
     }
 }
@@ -22,6 +24,9 @@ export class HandPose extends BasePose {
 
         this._myHandedness = handedness;
         this._myFixTrackedHandRotation = handPoseParams.myFixTrackedHandRotation;
+
+        this._mySwitchToTrackedHandDelay = handPoseParams.mySwitchToTrackedHandDelay;
+        this._mySwitchToTrackedHandTimer = new Timer(this._mySwitchToTrackedHandDelay, false);
 
         this._myTrackedHand = false;
 
@@ -49,7 +54,15 @@ export class HandPose extends BasePose {
     }
 
     setFixTrackedHandRotation(fixTrackedHandRotation) {
-        this.myFixTrackedHandRotation = fixTrackedHandRotation;
+        this._myFixTrackedHandRotation = fixTrackedHandRotation;
+    }
+
+    getSwitchToTrackedHandDelay() {
+        return this._mySwitchToTrackedHandDelay;
+    }
+
+    setSwitchToTrackedHandDelay(switchToTrackedHandDelay) {
+        this._mySwitchToTrackedHandDelay = switchToTrackedHandDelay;
     }
 
     getRotationQuat(out = quat_create(), referenceObjectOverride = undefined) {
@@ -64,17 +77,73 @@ export class HandPose extends BasePose {
         return xrFrame.getPose(this._myInputSource.gripSpace, this.getReferenceSpace());
     }
 
+    _preUpdate(dt) {
+        if (this._mySwitchToTrackedHandTimer.isRunning()) {
+            this._mySwitchToTrackedHandTimer.update(dt);
+            if (this._mySwitchToTrackedHandTimer.isDone()) {
+                if (this._myInputSourcesChangeEventListener != null) {
+                    const switchToTrackedHandDelayBackup = this._mySwitchToTrackedHandDelay;
+                    this._mySwitchToTrackedHandDelay = 0;
+                    this._myInputSourcesChangeEventListener();
+                    this._mySwitchToTrackedHandDelay = switchToTrackedHandDelayBackup;
+                }
+            }
+        }
+    }
+
     _onXRSessionStartHook(manualCall, session) {
         this._myInputSourcesChangeEventListener = () => {
             this._myInputSource = null;
+            this._myTrackedHand = false;
+            let resetSwitchToTrackedHandTimer = true;
 
             if (session.inputSources != null && session.inputSources.length > 0) {
+                let extraSourcesCheckForGamepad = false;
+
                 for (let i = 0; i < session.inputSources.length; i++) {
                     let inputSource = session.inputSources[i];
                     if (inputSource.handedness == this._myHandedness) {
+                        const inputSourceType = InputUtils.getInputSourceType(inputSource);
+                        const currentInputSourceType = this._myInputSource != null ? InputUtils.getInputSourceType(this._myInputSource) : null;
+
                         this._myInputSource = inputSource;
                         this._myTrackedHand = InputUtils.getInputSourceType(this._myInputSource) == InputSourceType.TRACKED_HAND;
+
+                        if (this._mySwitchToTrackedHandDelay > 0 && currentInputSourceType != InputSourceType.TRACKED_HAND && inputSourceType == InputSourceType.TRACKED_HAND) {
+                            extraSourcesCheckForGamepad = true;
+                        }
+
+                        break;
                     }
+                }
+
+                if (extraSourcesCheckForGamepad) {
+                    const sourcesToCheck = [];
+                    sourcesToCheck.push(...session.inputSources);
+                    if (session.trackedSources != null) {
+                        sourcesToCheck.push(...session.trackedSources);
+                    }
+
+                    for (const sourceToCheck of sourcesToCheck) {
+                        if (sourceToCheck.handedness == this._myHandedness) {
+                            const sourceToCheckType = InputUtils.getInputSourceType(sourceToCheck);
+                            if (sourceToCheckType == InputSourceType.GAMEPAD) {
+                                this._myInputSource = sourceToCheckType;
+                                this._myTrackedHand = false;
+
+                                resetSwitchToTrackedHandTimer = false;
+                                if (!this._mySwitchToTrackedHandTimer.isRunning()) {
+                                    this._mySwitchToTrackedHandTimer.start(this._mySwitchToTrackedHandDelay);
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (resetSwitchToTrackedHandTimer) {
+                    this._mySwitchToTrackedHandTimer.reset();
                 }
             }
         };
